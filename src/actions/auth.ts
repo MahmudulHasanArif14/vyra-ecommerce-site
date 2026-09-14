b3cc97860239d10e39372c6fe94f71c8339fa136
MahmudulHasanArif14 <arifmahmudulhasanlu84@gmail.com>
2026-09-14 20:43:46 +0100

    redirect fixed

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -80,8 +80,7 @@ export async function register(formData: FormData) {
 export async function signInWithGoogle() {
   const supabase = await createClient();
 
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
-
+  const siteUrl = process.env.SITE_URL || "http://localhost:3000";
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
